7250d0559facc9a69c97a6ef54798e69cc7aae2a
SangWook <imcool2551@gmail.com>
2022-02-11 15:51:02 +0900

    httpservlet request-header

--- servlet/src/main/java/hello/servlet/basic/HelloServlet.java
+++ servlet/src/main/java/hello/servlet/basic/HelloServlet.java
@@ -11,7 +11,17 @@ import java.io.IOException;
 public class HelloServlet extends HttpServlet {
 
     @Override
-    protected void service(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+    protected void service(HttpServletRequest req, HttpServletResponse res) throws ServletException, IOException {
         System.out.println("HelloServlet.service");
+        System.out.println("req = " + req);
+        System.out.println("res = " + res);
+
+        String username = req.getParameter("username");
+        System.out.println("username = " + username);
+
+        res.setContentType("text/plain");
+        res.setCharacterEncoding("utf-8");
+        res.getWriter().write("hello " + username);
+
     }
 }
