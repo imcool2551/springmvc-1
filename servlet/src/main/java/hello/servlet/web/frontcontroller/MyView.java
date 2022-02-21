d3219bd19e40451a4765cf99a29cf1e223f12fd8
SangWook <imcool2551@gmail.com>
2022-02-21 13:30:52 +0900

    front-controller V1

---
+++ servlet/src/main/java/hello/servlet/web/frontcontroller/MyView.java
@@ -0,0 +1,5 @@
+package hello.servlet.web.frontcontroller;
+
+public class MyView {
+
+}
--- servlet/src/main/java/hello/servlet/web/frontcontroller/v1/controller/MemberListControllerV1.java
+++ servlet/src/main/java/hello/servlet/web/frontcontroller/v1/controller/MemberListControllerV1.java
@@ -1,24 +1,22 @@
-package hello.servlet.web.servletmvc;
+package hello.servlet.web.frontcontroller.v1.controller;
 
 import hello.servlet.domain.member.Member;
 import hello.servlet.domain.member.MemberRepository;
+import hello.servlet.web.frontcontroller.v1.ControllerV1;
 
 import javax.servlet.RequestDispatcher;
 import javax.servlet.ServletException;
-import javax.servlet.annotation.WebServlet;
-import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
 import java.util.List;
 
-@WebServlet(name = "mvcMemberListServlet", urlPatterns = "/servlet-mvc/members")
-public class MvcMemberListServlet extends HttpServlet {
+public class MemberListControllerV1 implements ControllerV1 {
 
     private MemberRepository memberRepository = MemberRepository.getInstance();
 
     @Override
-    protected void service(HttpServletRequest req, HttpServletResponse res) throws ServletException, IOException {
+    public void process(HttpServletRequest req, HttpServletResponse res) throws ServletException, IOException {
         List<Member> members = memberRepository.findAll();
 
         req.setAttribute("members", members);
@@ -28,3 +26,4 @@ public class MvcMemberListServlet extends HttpServlet {
         dispatcher.forward(req, res);
     }
 }
+
